f1cf30cddb9fcb88642dcb0d4b793047c8c59369
Christian047 <126990667+Christian047@users.noreply.github.com>
2023-12-30 15:06:05 -0800

    Add files via upload

--- Data_visualization_app.py
+++ Data_visualization_app.py
@@ -1,21 +1,30 @@
-
-
 # Import relevant library
 import matplotlib.pyplot as plt  # Correct import statement
 
 # Assign values to x and y axis
-x = [2, 3, 4, 4]
-y = [5, 6, 7,11]
+x1 = [2, 3, 4, 4]
+y1 = [5, 6, 7, 11]
 
-# Plot the graph
-plt.plot(x, y)
+# Plot the first graph
+plt.plot(x1, y1, label='Line 1')
+
+x2 = [1, 3, 6, 12]
+y2 = [2, 6, 8, 10]
+
+# Plot the second graph
+plt.plot(x2, y2, label='Line 2')
 
 # Name the x and y axis
 plt.xlabel('X Axis')
 plt.ylabel('Y Axis')
 
 # Name the Graph title
-plt.title('Demo Graph')
+plt.title('Demo Graph - Two lines')
+
+# Add a legend for better readability
+plt.legend()
 
 # Show the graph
 plt.show()
+
+print('Done')
