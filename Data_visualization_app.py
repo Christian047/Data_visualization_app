c0b8bd409256f6af426c4d88709ba6dba5cec84a
Christian047 <126990667+Christian047@users.noreply.github.com>
2023-12-30 14:53:32 -0800

    Add files via upload

---
+++ Data_visualization_app.py
@@ -0,0 +1,21 @@
+
+
+# Import relevant library
+import matplotlib.pyplot as plt  # Correct import statement
+
+# Assign values to x and y axis
+x = [2, 3, 4, 4]
+y = [5, 6, 7,11]
+
+# Plot the graph
+plt.plot(x, y)
+
+# Name the x and y axis
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+
+# Name the Graph title
+plt.title('Demo Graph')
+
+# Show the graph
+plt.show()
